1ab4b743b2a9b41af7bdbc429990f9234aa71158
wchenfong <33014377+wchenfong@users.noreply.github.com>
2017-12-02 16:40:16 +0800

    Add files via upload

---
+++ FastestDeliveryMan-see order detail and amout need to pay/src/fastestdeliveryman/FastestDeliveryMan.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fastestdeliveryman;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import javax.swing.JComboBox;
+
+/**
+ *
+ * @author pc
+ */
+public class FastestDeliveryMan implements Serializable{
+
+    /**
+     * @param args the command line arguments
+     */
+    String restaurant;
+    String FoodName;
+
+   
+
+    
+
+    
+    public FastestDeliveryMan(String restaurant,String FoodName) {
+    this.restaurant = restaurant;
+    this.FoodName = FoodName;
+  }
+
+    FastestDeliveryMan(String foodname) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+    
+    public String getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(String Restaurant) {
+        this.restaurant = Restaurant;
+    }
+
+    public String getFoodName() {
+        return FoodName;
+    }
+
+    public void setFoodName(String FoodName) {
+        this.FoodName = FoodName;
+    }
+    
+
+      
+    public String toString() {
+    return String.format("",restaurant, FoodName);
+  }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
